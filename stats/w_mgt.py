e32e583f045ec2a59eaec48c306b1e445c85288f
Suhas K Vittal <svittal8@ampere01.cc.gatech.edu>
2025-01-22 09:25:37 -0500

    Added gap results

--- stats/w_mgt.py
+++ stats/w_mgt.py
@@ -27,7 +27,7 @@ def get_name(suite, filename):
 ####################################################################
 ####################################################################
 
-SUITES = ['mtf/spec2017']
+SUITES = ['mtf/spec2017','mtf/gap']
 
 def create_csv_file_for_ipc(output_file: str, *builds):
     wr = open(f'data/{output_file}', 'w')
